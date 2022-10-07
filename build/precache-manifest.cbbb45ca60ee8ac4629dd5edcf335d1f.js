a3bd40e21fd39d323177faa9e6452b375ee2d28a
ronakgala18 <ronakgala18@gmail.com>
2022-10-07 18:09:43 -0400

    Reach project added

---
+++ build/precache-manifest.cbbb45ca60ee8ac4629dd5edcf335d1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a122e80ce49810c37f57",
+    "url": "/ronakgala18/COMP3104/static/css/main.4ae98eca.chunk.css"
+  },
+  {
+    "revision": "a122e80ce49810c37f57",
+    "url": "/ronakgala18/COMP3104/static/js/main.a122e80c.chunk.js"
+  },
+  {
+    "revision": "2c5a230a115e06be8a36",
+    "url": "/ronakgala18/COMP3104/static/js/runtime~main.2c5a230a.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/ronakgala18/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "f9181687a7010db0c5789105f0c9db45",
+    "url": "/ronakgala18/COMP3104/index.html"
+  }
+];
